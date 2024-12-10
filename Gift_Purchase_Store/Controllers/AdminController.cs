7929073bc4d12ba06a23f4fd0fbeeb60831d3081
AkhilPrasanth <akhilprasanth007@gmail.com>
2024-12-10 14:26:06 +0530

    Admincontroller/View ready

---
+++ Gift_Purchase_Store/Controllers/AdminController.cs
@@ -0,0 +1,386 @@
+﻿using Gift_Purchase_Store.Data;
+using Gift_Purchase_Store.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static NuGet.Packaging.PackagingConstants;
+
+namespace Gift_Purchase_Store.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private Repository<Order> _orders;
+        private Repository<Product> products;
+        private Repository<Ingredient> ingredients;
+        private Repository<Category> categories;
+        
+
+        public AdminController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _userManager = userManager;
+            _webHostEnvironment = webHostEnvironment;
+            _orders = new Repository<Order>(context);
+            products = new Repository<Product>(context);
+            ingredients = new Repository<Ingredient>(context);
+            categories = new Repository<Category>(context);
+
+        }
+        //Product section
+        public async Task<IActionResult> ProductView()
+        {
+            return View(await products.GetAllAsync());
+        }
+
+
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> ProductAddEdit(int id)
+        {
+            ViewBag.Ingredients = await ingredients.GetAllAsync();
+            ViewBag.Categories = await categories.GetAllAsync();
+            if (id == 0)
+            {
+                ViewBag.Operation = "Add";
+                return View(new Product());
+            }
+            else
+            {
+                Product product = await products.GetByIdAsync(id, new QueryOptions<Product>
+                {
+                    Includes = "ProductIngredients.Ingredient, Category"
+                });
+                ViewBag.Operation = "Edit";
+                return View(product);
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> ProductAddEdit(Product product, int[] ingredientIds, int catId)
+        {
+            ViewBag.Ingredients = await ingredients.GetAllAsync();
+            ViewBag.Categories = await categories.GetAllAsync();
+            if (ModelState.IsValid)
+            {
+
+                if (product.ImageFile != null)
+                {
+                    string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + product.ImageFile.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await product.ImageFile.CopyToAsync(fileStream);
+                    }
+                    product.ImageUrl = uniqueFileName;
+                }
+
+                if (product.ProductId == 0)
+                {
+
+                    product.CategoryId = catId;
+
+                    //add ingredients
+                    foreach (int id in ingredientIds)
+                    {
+                        product.ProductIngredients?.Add(new ProductIngredient { IngredientId = id, ProductId = product.ProductId });
+                    }
+
+                    await products.AddAsync(product);
+                    return RedirectToAction("ProductView", "Admin");
+                }
+                else
+                {
+                    var existingProduct = await products.GetByIdAsync(product.ProductId, new QueryOptions<Product> { Includes = "ProductIngredients" });
+
+                    if (existingProduct == null)
+                    {
+                        ModelState.AddModelError("", "Product not found.");
+                        ViewBag.Ingredients = await ingredients.GetAllAsync();
+                        ViewBag.Categories = await categories.GetAllAsync();
+                        return View(product);
+                    }
+
+                    existingProduct.Name = product.Name;
+                    existingProduct.Description = product.Description;
+                    existingProduct.Price = product.Price;
+                    existingProduct.Stock = product.Stock;
+                    existingProduct.CategoryId = catId;
+
+                    // Update product ingredients
+                    existingProduct.ProductIngredients?.Clear();
+                    foreach (int id in ingredientIds)
+                    {
+                        existingProduct.ProductIngredients?.Add(new ProductIngredient { IngredientId = id, ProductId = product.ProductId });
+                    }
+
+                    try
+                    {
+                        await products.UpdateAsync(existingProduct);
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("", $"Error: {ex.GetBaseException().Message}");
+                        ViewBag.Ingredients = await ingredients.GetAllAsync();
+                        ViewBag.Categories = await categories.GetAllAsync();
+                        return View(product);
+                    }
+                }
+            }
+            return RedirectToAction("ProductView", "Admin");
+        }
+
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await products.DeleteAsync(id);
+                return RedirectToAction("ProductView");
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Product not found.");
+                return RedirectToAction("ProductView");
+            }
+        }
+        //Order Section
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            //ViewBag.Products = await _products.GetAllAsync();
+
+            //Retrieve or create an OrderViewModel from session or other state management
+            var model = HttpContext.Session.Get<OrderViewModel>("OrderViewModel") ?? new OrderViewModel
+            {
+                OrderItems = new List<OrderItemViewModel>(),
+                Products = await products.GetAllAsync()
+            };
+
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddItem(int prodId, int prodQty)
+        {
+            var product = await _context.Products.FindAsync(prodId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            // Retrieve or create an OrderViewModel from session or other state management
+            var model = HttpContext.Session.Get<OrderViewModel>("OrderViewModel") ?? new OrderViewModel
+            {
+                OrderItems = new List<OrderItemViewModel>(),
+                Products = await products.GetAllAsync()
+            };
+
+            // Check if the product is already in the order
+            var existingItem = model.OrderItems.FirstOrDefault(oi => oi.ProductId == prodId);
+
+            // If the product is already in the order, update the quantity
+            if (existingItem != null)
+            {
+                existingItem.Quantity += prodQty;
+            }
+            else
+            {
+                model.OrderItems.Add(new OrderItemViewModel
+                {
+                    ProductId = product.ProductId,
+                    Price = product.Price,
+                    Quantity = prodQty,
+                    ProductName = product.Name
+                });
+            }
+
+            // Update the total amount
+            model.TotalAmount = model.OrderItems.Sum(oi => oi.Price * oi.Quantity);
+
+            // Save updated OrderViewModel to session
+            HttpContext.Session.Set("OrderViewModel", model);
+
+            // Redirect back to Create to show updated order items
+            return RedirectToAction("Create", model);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Cart()
+        {
+
+            // Retrieve the OrderViewModel from session or other state management
+            var model = HttpContext.Session.Get<OrderViewModel>("OrderViewModel");
+
+            if (model == null || model.OrderItems.Count == 0)
+            {
+                return RedirectToAction("Create");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> PlaceOrder()
+        {
+            var model = HttpContext.Session.Get<OrderViewModel>("OrderViewModel");
+            if (model == null || model.OrderItems.Count == 0)
+            {
+                return RedirectToAction("Create");
+            }
+
+            // Create a new Order entity
+            Order order = new Order
+            {
+                OrderDate = DateTime.Now,
+                TotalAmount = model.TotalAmount,
+                UserId = _userManager.GetUserId(User)
+            };
+
+            // Add OrderItems to the Order entity
+            foreach (var item in model.OrderItems)
+            {
+                order.OrderItems.Add(new OrderItem
+                {
+                    ProductId = item.ProductId,
+                    Quantity = item.Quantity,
+                    Price = item.Price
+                });
+            }
+
+            // Save the Order entity to the database
+            await _orders.AddAsync(order);
+
+            // Clear the OrderViewModel from session or other state management
+            HttpContext.Session.Remove("OrderViewModel");
+
+            // Redirect to the Order Confirmation page
+            return RedirectToAction("ViewOrders");
+        }
+
+        [HttpGet]
+        [Authorize]
+        //public async Task<IActionResult> ViewOrders()
+        //{
+        //    var userId = _userManager.GetUserId(User);
+
+        //    var userOrders = await _orders.GetAllByIdAsync(userId, "UserId", new QueryOptions<Order>
+        //    {
+        //        Includes = "OrderItems.Product"
+        //    });
+
+        //    return View(userOrders);
+        //}
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Order()
+        {
+            var orders = await _context.Orders
+                .Include(o => o.User) // Include the user
+                .Include(o => o.OrderItems) // Include order items
+                .ThenInclude(oi => oi.Product) // Include products in order items
+                .Select(o => new OrderViewModel
+                {
+                    OrderId = o.OrderId,
+                    OrderDate = o.OrderDate,
+                    UserName = o.User.UserName, // Or any other property like FullName
+                    UserEmail = o.User.Email,
+                    TotalAmount = o.TotalAmount,
+                    OrderItems = o.OrderItems.Select(oi => new OrderItemViewModel
+                    {
+                        ProductId = oi.ProductId,
+                        ProductName = oi.Product.Name,
+                        Quantity = oi.Quantity,
+                        Price = oi.Price
+                    }).ToList()
+                })
+                .ToListAsync();
+
+            return View(orders);
+        }
+
+        //Type Section
+        public async Task<IActionResult> TypeView()
+        {
+            return View(await ingredients.GetAllAsync());
+        }
+
+        public async Task<IActionResult> TypeDetails(int id)
+        {
+            return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient>() { Includes = "ProductIngredients.Product" }));
+        }
+
+
+        //Ingredient/Create
+        [HttpGet]
+        public IActionResult TypeCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> TypeCreate([Bind("IngredientId, Name")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                await ingredients.AddAsync(ingredient);
+                return RedirectToAction("TypeView");
+            }
+            return View(ingredient);
+        }
+        //Ingredient/Delete
+        [HttpGet]
+        public async Task<IActionResult> TypeDelete(int id)
+        {
+            return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient> { Includes = "ProductIngredients.Product" }));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> TypeDelete(Ingredient ingredient)
+        {
+            await ingredients.DeleteAsync(ingredient.IngredientId);
+            return RedirectToAction("TypeView");
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> TypeEdit(int id)
+        {
+            return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient> { Includes = "ProductIngredients.Product" }));
+        }
+
+
+        [Authorize(Roles = "Admin")]
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> TypeEdit(Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                await ingredients.UpdateAsync(ingredient);
+                return RedirectToAction("TypeView");
+            }
+            return View(ingredient);
+        }
+    }
+}
